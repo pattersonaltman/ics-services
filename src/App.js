e70c773c0f5c539e64557de523b1ef33710b1ff7
Jalamang <Jalamangjanka@gmail.com>
2022-11-15 17:34:15 -0500

    installed react-router-dom and configured routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import React from 'react';
 // import Login from './components/login/Login'
 import Main from './components/Main';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Login from './components/login/Login';
+import Services from './components/services/Services';
+import Register from './components/register/Register';
+import ServiceDetails from './components/serviceDetails/ServiceDetails';
+import ServiceCard from './components/serviceCard/ServiceCard';
+
 function App() {
   return (
     <div className="App">
-      <Main/>
-      {/* <Login/> */}
+   <Navbar/>
+ <Routes>
+  <Route path="/" element={<Main/>}></Route>
+  <Route path="/dish" element={<Login/>}></Route>
+  <Route path="/register" element={<Register/>}></Route>
+  <Route path="/services" element={<Services/>}></Route>
+  <Route path="/services/:id" element={<ServiceDetails/>}></Route>
+  <Route path="/servicecard" element={<ServiceCard/>}></Route>
+ </Routes>
     </div>
   );
 }
